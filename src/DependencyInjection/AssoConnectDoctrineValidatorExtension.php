45ab4ea64c3cd2297c89d7a9c667589d8fb89560
Guillaume Wambre <guillaume.wambre@assoconnect.com>
2020-03-16 15:22:02 +0100

    Corrections

--- src/DependencyInjection/AssoConnectDoctrineValidatorExtension.php
+++ src/DependencyInjection/AssoConnectDoctrineValidatorExtension.php
@@ -8,37 +8,8 @@ use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-class AssoConnectDoctrineValidatorExtension extends Extension implements PrependExtensionInterface
+class AssoConnectDoctrineValidatorExtension extends Extension
 {
-    public function prepend(ContainerBuilder $container)
-    {
-        $container->prependExtensionConfig('doctrine', [
-            'dbal' => [
-                'mapping_types' => [
-                    'bic' => 'string',
-                    'country' => 'string',
-                    'currency' => 'string',
-                    'datetimetz' => 'datetime',
-                    'datetimeutc' => 'datetime',
-                    'email' => 'string',
-                    'iban' => 'string',
-                    'ip' => 'string',
-                    'latitude' => 'decimal',
-                    'locale' => 'string',
-                    'longitude' => 'decimal',
-                    'money' => 'decimal',
-                    'percent' => 'decimal',
-                    'phone' => 'string',
-                    'phonelandline' => 'string',
-                    'phonemobile' => 'string',
-                    'postal' => 'string',
-                    'timezone' => 'string',
-                    'uuid_binary_ordered_time' => 'binary',
-                ],
-            ]
-        ]);
-    }
-
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new YamlFileLoader(
