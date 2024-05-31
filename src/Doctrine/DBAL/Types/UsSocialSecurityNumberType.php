d04d84638e9ae057b4b242a911c42b3861d28577
Florian Guimier <florian.guimier@assoconnect.com>
2024-05-31 17:36:26 +0200

    Add UsSocialSecurityNumberType

---
+++ src/Doctrine/DBAL/Types/UsSocialSecurityNumberType.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AssoConnect\DoctrineTypesBundle\Doctrine\DBAL\Types;
+
+class UsSocialSecurityNumberType extends AbstractFixedLengthStringType
+{
+    public const NAME = 'usSocialSecurityNumber';
+    public const LENGTH = 9;
+
+    public function getName(): string
+    {
+        return self::NAME;
+    }
+
+    protected function getLength(): int
+    {
+        return self::LENGTH;
+    }
+}
